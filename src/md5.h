3c21b08554925e3cd82baed4270c2e8531641c26
Mathieu Schroeter <mathieu.schroeter@mycable.ch>
2009-08-10 15:04:41 +0200

    import md5 algo provided by FFmpeg

---
+++ src/md5.h
@@ -0,0 +1,27 @@
+/*
+ * GeeXboX Valhalla: tiny media scanner API.
+ * Copyright (C) 2009 Mathieu Schroeter <mathieu.schroeter@gamesover.ch>
+ *
+ * This file is part of libvalhalla.
+ *
+ * libvalhalla is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * libvalhalla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with libvalhalla; if not, write to the Free Software
+ * Foundation, Inc, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef VALHALLA_MD5_H
+#define VALHALLA_MD5_H
+
+char *md5sum (const char *str);
+
+#endif /* VALHALLA_MD5_H */
