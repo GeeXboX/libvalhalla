d57fdab61709160b6740fb16443150e047738702
Mathieu Schroeter <mathieu.schroeter@mycable.ch>
2009-09-07 20:29:01 +0200

    fix memleak with get_prop_value_from_xml_tree_by_attr()

--- src/xml_utils.c
+++ src/xml_utils.c
@@ -126,8 +126,12 @@ get_prop_value_from_xml_tree_by_attr (xmlNode *root, const char *prop,
       continue;
 
     if (xmlStrcmp ((unsigned char *) attr_value, content))
+    {
+      xmlFree (content);
       continue;
+    }
 
+    xmlFree (content);
     return xmlNodeGetContent (n);
   }
 
