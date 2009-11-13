3e938a43d3224da8263cff72211e080e42244e45
Mathieu Schroeter <mathieu.schroeter@mycable.ch>
2009-11-13 22:36:49 +0100

    add LGPLv2.1+ license header to PHP scripts

--- utils/searchMovieAllocine.php
+++ utils/searchMovieAllocine.php
@@ -1,4 +1,24 @@
 <?php
+/*
+ * GeeXboX Valhalla: tiny media scanner API.
+ * Copyright (C) 2009 Fabien Brisset <fbrisset@gmail.com>
+ *
+ * This file is part of libvalhalla.
+ *
+ * libvalhalla is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * libvalhalla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with libvalhalla; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
     /* set the content type to be XML, so that the browser will recognise it as XML */
     header ("content-type: application/xml; charset=utf-8");
--- utils/searchMovieIMDB.php
+++ utils/searchMovieIMDB.php
@@ -1,4 +1,26 @@
 <?php
+/*
+ * GeeXboX Valhalla: tiny media scanner API.
+ * Copyright (C) 2009 Fabien Brisset <fbrisset@gmail.com>
+ * Copyright (C) 2009 Davide Cavalca <davide@geexbox.org>
+ *
+ * This file is part of libvalhalla.
+ *
+ * libvalhalla is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * libvalhalla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with libvalhalla; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
     /* This needs imdbphp (http://projects.izzysoft.de/trac/imdbphp) to work */
     require_once('imdbphp/imdb.class.php');
 
