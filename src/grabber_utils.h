9100a2c746321b7ee1cd38f1ac316a1eb76f0b04
Mathieu Schroeter <mathieu.schroeter@mycable.ch>
2009-11-15 15:51:13 +0100

    only cosmetics

--- src/grabber_utils.h
+++ src/grabber_utils.h
@@ -26,15 +26,13 @@
 #include "xml_utils.h"
 #endif /* USE_XML */
 
-void vh_grabber_parse_int (file_data_t *fdata, int val,
-                           const char *name);
+void vh_grabber_parse_int (file_data_t *fdata, int val, const char *name);
 void vh_grabber_parse_float (file_data_t *fdata, float val, const char *name);
 
 #ifdef USE_XML
-void vh_grabber_parse_str (file_data_t *fdata, xmlNode *nd, const char *tag,
-                           const char *name);
+void vh_grabber_parse_str (file_data_t *fdata,
+                           xmlNode *nd, const char *tag, const char *name);
 void vh_grabber_parse_categories (file_data_t *fdata, xmlNode *node);
-
 void vh_grabber_parse_casting (file_data_t *fdata, xmlNode *node);
 #endif /* USE_XML */
 
