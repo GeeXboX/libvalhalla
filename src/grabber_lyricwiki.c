f9aca09e03172cc9cb5186a2c3856b57c38508a4
Mathieu Schroeter <mathieu.schroeter@mycable.ch>
2010-01-16 11:39:59 +0100

    ensure that the last byte is null even if the string in 'lyrics' is always smaller that 'txt'

--- src/grabber_lyricwiki.c
+++ src/grabber_lyricwiki.c
@@ -135,7 +135,7 @@ grabber_lyricwiki_get (grabber_lyricwiki_t *lyricwiki, file_data_t *fdata,
     len = end - offset;
     txt = strndup (offset, len);
 
-    lyrics = calloc (1, len);
+    lyrics = calloc (1, len + 1);
     for (i = 0, j = 0; i < (unsigned int) len; i++)
     {
       int n = 0;
