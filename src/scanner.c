9f139fa24577faab3d7e2b9323babfc53210aba0
Mathieu Schroeter <mathieu.schroeter@mycable.ch>
2010-02-13 13:34:05 +0100

    fix constant type for uint64_t

--- src/scanner.c
+++ src/scanner.c
@@ -365,7 +365,7 @@ vh_scanner_run (scanner_t *scanner, int loop, uint16_t timeout, int priority)
   /* if timeout is 0, there is no sleep between loops */
   if (timeout)
   {
-    scanner->timeout = timeout * 1000000000UL;
+    scanner->timeout = timeout * 1000000000ULL;
     vh_timer_thread_start (scanner->timer);
   }
 
