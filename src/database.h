27dd1a2e70877e3f5536c5444b8b2380712fd6b2
Mathieu Schroeter <mathieu.schroeter@mycable.ch>
2010-02-25 10:50:48 +0100

    updated copyright date of some files

--- src/database.h
+++ src/database.h
@@ -1,6 +1,6 @@
 /*
  * GeeXboX Valhalla: tiny media scanner API.
- * Copyright (C) 2009 Mathieu Schroeter <mathieu.schroeter@gamesover.ch>
+ * Copyright (C) 2009-2010 Mathieu Schroeter <mathieu.schroeter@gamesover.ch>
  *
  * This file is part of libvalhalla.
  *
