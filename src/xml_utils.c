8904eedee64dcfdb343946c2c128f6a5b2d75a35
Mathieu Schroeter <mathieu@schroetersa.ch>
2012-03-21 18:08:04 +0100

    update copyright date

--- src/xml_utils.c
+++ src/xml_utils.c
@@ -1,6 +1,6 @@
 /*
  * GeeXboX Valhalla: tiny media scanner API.
- * Copyright (C) 2009 Mathieu Schroeter <mathieu@schroetersa.ch>
+ * Copyright (C) 2009-2012 Mathieu Schroeter <mathieu@schroetersa.ch>
  *
  * This file is part of libvalhalla.
  *
